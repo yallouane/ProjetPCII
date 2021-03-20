fa39496c40c482c6f21d32b22368e613c59ced7f
Mizzark <33715653+Mizzark@users.noreply.github.com>
2021-03-20 13:04:47 +0100

    Update Deplacement.java

--- controleur/Deplacement.java
+++ controleur/Deplacement.java
@@ -7,46 +7,58 @@ import modele.Vehicule;
 import vue.Vue;
 
 public class Deplacement implements KeyListener {
-	
-	private Vue affichage;
-	private Vehicule vehicule;
-	
-	public Deplacement(Vue vue, Vehicule vehicule) {
-		this.affichage = vue;
-		this.vehicule = vehicule;
-	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == 38 || e.getKeyCode() == 90) { // HAUT
-			vehicule.deplacement("HAUT");
-			affichage.repaint();
-		}
-		else if(e.getKeyCode() == 40 || e.getKeyCode() == 83) { // BAS
-			vehicule.deplacement("BAS");
-			affichage.repaint();
-		}
-		else if(e.getKeyCode() == 37 || e.getKeyCode() == 81) { // GAUCHE
-			vehicule.deplacement("GAUCHE");
-			affichage.repaint();
-		}
-		else if(e.getKeyCode() == 39 || e.getKeyCode() == 68) { // DROITE
-			vehicule.deplacement("DROITE");
-			affichage.repaint();
-		}
-	}
+    private Vue affichage;
+    private Vehicule vehicule;
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Deplacement(Vue vue, Vehicule vehicule) {
+        this.affichage = vue;
+        this.vehicule = vehicule;
+    }
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case 38:
+            case 90:
+                // HAUT
+                vehicule.deplacement("HAUT");
+                affichage.repaint();
+                break;
+            case 40:
+            case 83:
+                // BAS
+                vehicule.deplacement("BAS");
+                affichage.repaint();
+                break;
+            case 37:
+            case 81:
+                // GAUCHE
+                vehicule.deplacement("GAUCHE");
+                affichage.repaint();
+                break;
+            case 39:
+            case 68:
+                // DROITE
+                vehicule.deplacement("DROITE");
+                affichage.repaint();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
+
