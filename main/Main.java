f06b157331434721c14e346f099d06507798e905
Mizzark <33715653+Mizzark@users.noreply.github.com>
2021-03-20 13:06:36 +0100

    Add files via upload

--- main/Main.java
+++ main/Main.java
@@ -5,23 +5,26 @@ import javax.swing.JFrame;
 import controleur.Deplacement;
 import vue.*;
 import modele.*;
+import modele.thread.Defilement;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Route route = new Route();
-		route.genererLigne();
-		Vehicule vehicule = new Vehicule(Vue.OVAL_X, Vue.OVAL_Y);
-		Modele modele = new Modele(vehicule, route);
-		Vue affichage = new Vue(modele);
-		Deplacement deplacement = new Deplacement(affichage,vehicule);
-		affichage.addKeyListener(deplacement);
-		JFrame test = new JFrame("Course");
-	    test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    test.add(affichage);
-	    test.pack();
-	    test.setVisible(true);
-	    
-	}
+    public static void main(String[] args) {
+        Route route = new Route();
+        route.genererLigne();
+        Vehicule vehicule = new Vehicule(Vue.OVAL_X, Vue.OVAL_Y);
+        Modele modele = new Modele(vehicule, route);
+        Vue affichage = new Vue(modele);
+        modele.setVue(affichage);
+        Deplacement deplacement = new Deplacement(affichage, vehicule);
+        Defilement defilement = new Defilement(modele);
+        affichage.addKeyListener(deplacement);
+        JFrame test = new JFrame("Course");
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        test.add(affichage);
+        test.pack();
+        test.setVisible(true);
+
+    }
 
 }
